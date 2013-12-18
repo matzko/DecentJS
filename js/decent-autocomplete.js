77e4b09e141604f240cdd3897ffa8a92c067d11b
Austin Matzko <austin@pressedcode.com>
2013-12-18 17:32:17 -0500

    Rework autocomplete to SelectionItem objects, for better extensibility.

--- js/decent-autocomplete.js
+++ js/decent-autocomplete.js
@@ -17,7 +17,7 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 	canFetchList = true,
 	requestCache = {},
 	requestListData = function(subject, callback) {
-		callback.call(subject, subject.value, 
+		callback.call(djs, subject.value, 
 			(function(queryText) {
 				return function(data) {
 					setListData(queryText, data);
@@ -36,36 +36,41 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 	},
 	list = create('ul'),
 	listData = {},
+
 	/**
-	 * Determine whether the given word matches the given text.
+	 * Sort the giving selection items.  -this- contains the relevant search term to consider.
 	 *
-	 * @param [string] word        The word to match.
-	 * @param [string] operandText The text to find the match in.
-	 * @return [boolean] Whether the word matches.
-	 **/
-	wordMatches = function(word, operandText) {
-		word = word + '';
-		var re = new RegExp(word.split('').join('\\w*').replace(/\W/, ""), 'i');
-		return !! operandText.match(re);
-	},
-	matchingListSort = function(a, b) {
+	 * @param [SelectionItem] a One item to compare.
+	 * @param [SelectionItem] b Another item to compare.
+	 *
+	 * @return [integer] The sorting result: -1 if a should be first; 1 if b; 0 if equal.
+	 */
+	matchingItemsSort = function(a, b) {
 		var searchTerm = this,
-		getMatchLevel = function(searchResult, searchTerm) {
+		/**
+		 * Get the matching level for the given selection item.
+		 *
+		 * @param [SelectionItem] item       The selection item to match.
+		 * @param [String]        searchTerm The search term to match.
+		 *
+		 * @return [integer] The matching level of the selection item.
+		 */
+		getMatchLevel = function(item, searchTerm) {
 			var level = 0;
 			// Best: beginning with the same word
-			if (!!searchResult.match(new RegExp('^' + searchTerm, 'i'))) {
+			if (item.matches(new RegExp('^' + searchTerm, 'i'))) {
 				level = 3;
 			// Better: containing the actual word
-			} else if (!!searchResult.match(new RegExp(searchTerm,'i'))) {
+			} else if (item.matches(new RegExp(searchTerm,'i'))) {
 				level = 2;
 			// Somewhat preferred: shared characters
-			} else if (!!searchResult.match(new RegExp(searchTerm.split('').join('\\w*').replace(/\W/, ""), 'i'))) {
+			} else if (item.matches(new RegExp(searchTerm.split('').join('\\w*').replace(/\W/, ""), 'i'))) {
 				level = 1;
 			}
 			return level;
 		},
-		matchLevelA = getMatchLevel(a.text, searchTerm),
-		matchLevelB = getMatchLevel(b.text, searchTerm);
+		matchLevelA = getMatchLevel(a, searchTerm),
+		matchLevelB = getMatchLevel(b, searchTerm);
 
 		if (matchLevelA > matchLevelB) {
 			return -1;
@@ -73,7 +78,7 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 			return 1;
 		// if the same, sort alphabetically
 		} else {
-			return a.text < b.text ? -1 : 1;
+			return a.getPlainText() < b.getPlainText() ? -1 : 1;
 		}
 	},
 	coreOptions = {
@@ -82,8 +87,15 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 		ghostFill:1,
 		maxShow:10,
 		minChars:3,
-		resultSort:matchingListSort
+		matchingItemsSort:matchingItemsSort
 	},
+
+	/**
+	 * Create a subset of the data that matched the given word.
+	 *
+	 * @param [String] word The word to match.
+	 * @param [Object] data The matching SelectionItems associated by their Ids.
+	 */
 	buildMatchingListFromData = function(word, data) {
 		var i,
 		j = list.children.length,
@@ -96,24 +108,25 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 			list.removeChild(list.children[j]);
 		}
 		for(i in data) {
-			if (data[i] && data[i].text && wordMatches(word, data[i].text)) {
+			if (
+				data[i] 
+				&& (data[i] instanceof djs.autocomplete.SelectionItem)
+				&& data[i].matches(new RegExp(('' + word).split('').join('\\w*').replace(/\W/, ""), 'i'))
+			) {
+				data[i].setMatchingWord(word);
 				matchingData[matchingData.length] = data[i];
 			}
 		}
 		matchingData = matchingData.sort(function(a, b) {
-			return coreOptions['resultSort'].call(word, a, b);
+			return coreOptions['matchingItemsSort'].call(word, a, b);
 		}).slice(0,coreOptions['maxShow']);
 		for (i = 0; i < matchingData.length; i++) {
 			linkId = 'autocomplete-item-link-' + (i+1);
-			item = makeListItem(matchingData[i].text, linkId);
+			item = makeListItem(matchingData[i].getDisplayMarkup(), linkId);
+			matchingData[i].setListItemNode(item);
+
 			// selection index 0 is nothing
-			selections[i + 1] = {
-				id:matchingData[i].id,
-				origText:matchingData[i].text,
-				text:matchingData[i].text,
-				datum:matchingData[i],
-				node:item
-			};
+			selections[i + 1] = matchingData[i];
 			selectionLinks[linkId] = (i + 1);
 			list.appendChild(item);
 		}
@@ -121,85 +134,93 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 			showList();
 		}
 	},
-	makeListItem = function(textValue, id) {
-		var content = djs.doc.createTextNode(textValue),
-		listItem = create('li'),
+
+	/**
+	 * Make a list item from the given markup.
+	 *
+	 * @param [string] markupValue The markup for the content of the item.
+	 * @param [string] id          The id of the list item.
+	 * 
+	 * @return [DOMElement] The <li> list item.
+	 */
+	makeListItem = function(markupValue, id) {
+		var listItem = create('li'),
 		link = create('a');
 		link.id = id;
 		link.className = 'autocomplete-item-link';
-		link.appendChild(content);
+		link.innerHTML = markupValue;
 		listItem.appendChild(link);
 		listItem.className = 'autocomplete-item';
 		return listItem;
 	},
+
+	/**
+	 * Add the response data for the given query.
+	 *
+	 * @param [String] queryText The text query that resulted in the given data.
+	 * @param [Array]  data      The data from the response.
+	 *  data can be an array of either Strings or SelectionItems
+	 */
 	setListData = function(queryText, data) {
 		data = data || [];
-		var i, id, text, callback;
+		var i;
 		for (i = 0; i < data.length; i++) {
-			if (data[i]) {
-				text = ('object' == (typeof data[i])) && data[i].text ? data[i].text : '' + data[i];
-				id = ('object' == (typeof data[i])) && data[i].id ? data[i].id : text;
-				callback = ('object' == (typeof data[i])) && ('function' == (typeof data[i].callback)) ? data[i].callback : function(){};
-				listData[id] = {id:id,text:text,callback:callback};
-				requestCache[text] = 1;
+			if (data[i] && data[i] instanceof djs.autocomplete.SelectionItem) {
+				listData[data[i].getId()] = data[i];
+				requestCache[data[i].getPlainText()] = 1;
+			} else if (data[i] && 'string' == (typeof data[i]).toLowerCase()) {
+				listData[data[i]] = new djs.autocomplete.SelectionItem({id:data[i],text:data[i]});
+				requestCache[data[i]] = 1;
 			}
 		} 
 		if (0 < data.length) {
 			requestCache[queryText] = 1;
 		}
 	},
-	matchCurrentText = function(text, selections) {
+
+	/**
+	 * Get the index of the selection item that matches the given text.
+	 *
+	 * @param [String] text       The text to match.
+	 * @param [Array]  selections The selection items in which to identify matching text.
+	 *
+	 * @return [Integer] The index of the first matching selection.
+	 **/
+	findFirstMatchingSelectionItem = function(text, selections) {
 		var firstTextMatch = null, i,
-		re = new RegExp('^(' + text + ')'),
-		match = null;
+		re = new RegExp('^(' + text + ')');
 		for(i = 1; i < selections.length; i++) {
-			if (selections[i] && selections[i]['origText']) {
-				match = re.exec(selections[i]['origText']);
-				if (match) {
-					if (null === firstTextMatch) {
-						firstTextMatch = i;
-					}
-					selections[i]['text'] = selections[i]['origText'].replace(re, '<strong class="matched-text">$1</strong>');
-				} else {
-					selections[i]['text'] = selections[i]['origText'];
-				}
-				if (selections[i]['node'] && selections[i]['node'].firstChild) {
-					selections[i]['node'].firstChild.innerHTML = selections[i]['text']; 
-				}
+			if (selections[i] && re.exec(selections[i].getPlainText())) {
+				firstTextMatch = i;
 			}
 		}
 		return firstTextMatch;
 	},
+
 	/**
 	 * Make the current selection to be the choice.
 	 */
 	chooseSelection = function(index) {
-		if (selections[index] && selections[index].datum && selections[index].datum) {
-			selections[index].datum.callback.call(selections[index].datum, 'choice');
+		if (selections[index]) {
+			selections[index].trigger('chosen');
 		}
 	},
 	setSelection = function(nextPosition) {
-		if (selections[activeSelection] && selections[activeSelection]['node']) {
-			d.removeClass(selections[activeSelection]['node'], 'state-focus');
+		if (selections[activeSelection] && selections[activeSelection].getListItemNode()) {
+			d.removeClass(selections[activeSelection].getListItemNode(), 'state-focus');
 		}
 		activeSelection = ((nextPosition >= selections.length) || (1 > nextPosition) ? 0 : nextPosition);
-		if (selections[activeSelection] && selections[activeSelection]['node']) {
-			d.addClass(selections[activeSelection]['node'], 'state-focus');
+		if (selections[activeSelection] && selections[activeSelection].getListItemNode()) {
+			d.addClass(selections[activeSelection].getListItemNode(), 'state-focus');
 		}
 		if (null === originalText && null === generatedText) {
 			originalText = subject.value + '';
 		}
-		if (activeSelection && selections[activeSelection] && selections[activeSelection]['origText']) {
-			if (selections[activeSelection].datum && selections[activeSelection].datum.callback) {
-				selections[activeSelection].datum.callback.call(selections[activeSelection].datum, 'selection');
-			}
-			subject.value = generatedText = selections[activeSelection]['origText'];
+		if (activeSelection && selections[activeSelection]) {
+			selections[activeSelection].trigger('selected');
+			subject.value = generatedText = selections[activeSelection].getPlainText();
 			if (coreOptions['ghostFill']) {
-				if (selections[activeSelection] && selections[activeSelection]['origText']) {
-					ghost.value = selections[activeSelection]['origText'];
-				} else {
-					ghost.value = '';
-				}
+				ghost.value = selections[activeSelection].getPlainText();
 			}
 		} else {
 			subject.value = null === originalText ? '' : originalText;
@@ -277,9 +298,9 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 				ghost.value = '';
 				ghostMatch = 0;
 				if (text && 0 < selections.length) {
-					index = matchCurrentText(text, selections);
-					if (index && selections[index] && selections[index]['text']) {
-						ghost.value = selections[index]['origText'];
+					index = findFirstMatchingSelectionItem(text, selections);
+					if (index && selections[index]) {
+						ghost.value = selections[index].getPlainText();
 					}
 					ghostMatch = index;
 				}
@@ -327,6 +348,216 @@ DecentJS.core.prototype.autocomplete = function(callback,options) {
 		hideList();
 		container.appendChild(list);
 	}
+	return this;
+}
+DecentJS.core.prototype.autocomplete.SelectionItem = function(args) {
+	var priorText, plainText, displayText, id,
+	matchingWord, associatedNode,
+	events = {};
+
+	/**
+	 * Construct the values.
+	 *
+	 * @param [Object] The initializer.
+	 */
+	this.init = function(args) {
+		args = args || {};
+		priorText = plainText = args['text'] || '';
+		displayText = args['displayText'] || null;
+		id = args['id'] || null;
+	}
+
+	/**
+	 * Add an event listener.
+	 *
+	 * @param [String]   eventName The name of the event.
+	 * @param [function] callback  The callback listener,
+	 * which receives the event name as its first parameter
+	 * and the SelectionItem as -this-.
+	 *
+	 * @return [String] The Id of the callback.
+	 */
+	this.attachListener = function(eventName, callback) {
+		if (!events[eventName]) {
+			events[eventName] = [];
+		}
+		events[eventName][events[eventName].length] = callback;
+		return eventName + '_' + (events[eventName].length - 1);
+	}
+
+	/**
+	 * Trigger a selection item event.
+	 *
+	 * @param [String] eventName       The name of the event.
+	 * @param [Array]  inputParameters Optional parameters to pass to the callback.
+	 *
+	 * @return [SelectionItem] The selection item.
+	 */
+	this.trigger = function(eventName, inputParameters) {
+		inputParameters = inputParameters || [];
+		var callbackCount = events[eventName] ? events[eventName].length : 0,
+		parameters = [eventName],
+		i;
+		for(i = 0; i < inputParameters.length; i++) {
+			parameters[parameters.length] = inputParameters[i];
+		}
+		for(i = 0; i < callbackCount; i++) {
+			if (events[eventName][i]) {
+				events[eventName][i].apply(this, parameters);
+			}
+		}
+		return this;
+	}
+
+	/**
+	 * Set the Id of the selection item.
+	 *
+	 * @param [String] value The Id of the selection item.
+	 * @return [SelectionItem] The selection item.
+	 */
+	this.setId = function(value) {
+		id = value;
+		return this;
+	}
+
+	/**
+	 * Get the Id of the selection item.
+	 *
+	 * @return [String] The Id.
+	 */
+	this.getId = function() {
+		return id;
+	}
+
+	/**
+	 * Set the plain text value of the selection item.
+	 *
+	 * @param [String] value The value of the plain text.
+	 * @return [SelectionItem] The selection item.
+	 */
+	this.setPlainText = function(value) {
+		plainText = value;
+		return this;
+	}
+
+	/**
+	 * Get the plain text form of the item value,
+	 * which can be used, e.g., in the input
+	 * element's value attribute.
+	 *
+	 * @return [String] The plain text item value.
+	 */
+	this.getPlainText = function() {
+		return plainText;
+	}
+
+	/**
+	 * Set the display markup.
+	 *
+	 * @param [String] value The HTML text value of the display markup.
+	 * @return [SelectionItem] The selection item.
+	 */
+	this.setDisplayMarkup = function(value) {
+		displayText = value;
+		return this;
+	}
+
+	/**
+	 * Get the display markup, used in the autocomplete dropdown.
+	 * Defaults to the plain text value.
+	 *
+	 * @return [String] The markup for the dropdown value of the item.
+	 */
+	this.getDisplayMarkup = function() {
+	/*
+				if (selections[i]['node'] && selections[i]['node'].firstChild) {
+					selections[i]['node'].firstChild.innerHTML = selections[i]['text']; 
+				}
+				/**/
+		return null === displayText ? this.getPlainText() : displayText;
+	}
+
+	/**
+	 * Set the word that matches the selection item.
+	 *
+	 * @param [string] word The word that matches.
+	 *
+	 * @return [SelectionItem] The selection item.
+	 */
+	this.setMatchingWord = function(word) {
+		matchingWord = word;
+		return this;
+	}
+
+	/**
+	 * Get the word that matches the selection item.
+	 *
+	 * @return [string] The word that matches the selection item.
+	 */
+	this.getMatchingWord = function() {
+		return matchingWord;
+	}
+
+	/**
+	 * Set the DOMElement list item node that is associated with this selection item.
+	 *
+	 * @param [DOMElement] node The DOM element that is associated with the selection item.
+	 *
+	 * @return [SelectionItem] The selection item.
+	 */
+	this.setListItemNode = function(node) {
+		associatedNode = node;
+		return this;
+	}
+
+	/**
+	 * Get the DOMElement list item node that is associated with this selection item.
+	 *
+	 * @return [DOMElement] The associated <li> item.
+	 */
+	this.getListItemNode = function() {
+		return associatedNode;
+	}
+
+	/**
+	 * Determine whether the selection item matches.
+	 *
+	 * @param [Object] matcher The String, RegExp, or callback that determines whether the selection item matches.
+	 *
+	 * @return [boolean] Whether the selection item matches.
+	 */
+	this.matches = function(matcher) {
+		switch((typeof matcher).toLowerCase()) {
+			case "string" :
+				return !! this.getPlainText().match(new RegExp(matcher));
+				break;
+			case "function" :
+				return !! matcher.call(this, this.getPlainText());
+				break;
+			case "object" :
+				if (matcher instanceof RegExp) {
+					return !! this.getPlainText().match(matcher);
+				}
+				break;
+		}
+	}
+
+	/**
+	 * If the item matches the given text, trigger the "matched" event.
+	 *
+	 * @param [String] matcher The matcher to match.
+	 *
+	 * @return [boolean] Whether the given matcher matches the selection item.
+	 */
+	this.reactIfMatch = function(matcher) {
+		if (this.matches(matcher)) {
+			this.trigger('matched', matcher);
+			return true;
+		}
+		return false;
+	}
+	
+	this.init(args);
 }
 }
 })(this);
